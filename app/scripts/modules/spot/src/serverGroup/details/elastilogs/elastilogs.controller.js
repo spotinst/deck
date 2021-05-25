99ee053ed8138c026028b079c54c4e2d2a71b44e
sidaniel <si.danielles@gmail.com>
2021-05-25 11:21:32 +0300

    [src] Filter Elastilogs

--- app/scripts/modules/spot/src/serverGroup/details/elastilogs/elastilogs.controller.js
+++ app/scripts/modules/spot/src/serverGroup/details/elastilogs/elastilogs.controller.js
@@ -2,11 +2,44 @@
 import { module } from 'angular';
 
 import { ServerGroupReader } from '@spinnaker/core';
+import moment from 'moment';
 
 export const SPOT_SERVERGROUP_DETAILS_ELASTILOGS_CONTROLLER =
   'spinnaker.spot.serverGroup.details.elastilogs.elastilogs.controller';
 export const name = SPOT_SERVERGROUP_DETAILS_ELASTILOGS_CONTROLLER; // for backwards compatibility
-module(SPOT_SERVERGROUP_DETAILS_ELASTILOGS_CONTROLLER, []).controller('spotElastilogsCtrl', [
+export const ALL = 'ALL';
+export const INFO = 'INFO';
+export const DEBUG = 'DEBUG';
+export const WARN = 'WARN';
+export const ERROR = 'ERROR';
+export const ONE_DAY = 'ONE DAY';
+export const TWO_DAYS = 'TWO DAYS';
+export const THREE_DAYS = 'THREE DAYS';
+export const ONE_WEEK = 'ONE WEEK';
+export const TWO_WEEKS = 'TWO WEEKS';
+export const ONE_MONTH = 'ONE MONTH';
+export const TWO_MONTHS = 'TWO MONTHS';
+export const THREE_MONTHS = 'THREE MONTHS';
+const mode = module(SPOT_SERVERGROUP_DETAILS_ELASTILOGS_CONTROLLER, []);
+mode
+  .constant({
+    ONE_DAY,
+    TWO_DAYS,
+    THREE_DAYS,
+    ONE_WEEK,
+    TWO_WEEKS,
+    ONE_MONTH,
+    TWO_MONTHS,
+    THREE_MONTHS,
+  })
+  .constant({
+    ALL,
+    INFO,
+    DEBUG,
+    WARN,
+    ERROR,
+  });
+mode.controller('spotElastilogsCtrl', [
   '$scope',
   '$uibModalInstance',
   'action',
@@ -16,6 +49,12 @@ module(SPOT_SERVERGROUP_DETAILS_ELASTILOGS_CONTROLLER, []).controller('spotElast
     this.action = action;
     this.serverGroup = serverGroup;
     this.application = application;
+    $scope.severity = 'ALL';
+    $scope.time = 'ONE_DAY';
+    $scope.timeLabel = ONE_DAY;
+    const now = moment();
+    $scope.toDate = now.format();
+    $scope.fromDate = now.add(-1, 'days').format();
 
     this.close = function() {
       $uibModalInstance.close();
@@ -23,6 +62,54 @@ module(SPOT_SERVERGROUP_DETAILS_ELASTILOGS_CONTROLLER, []).controller('spotElast
 
     this.handleElastilogs = function(elastilogs) {
       this.elastilogs = elastilogs;
+    };
+
+    this.setElastilogsSeverity = function(severity) {
+      $scope.severity = severity;
+      this.getElastilogs();
+    };
+
+    this.setElastilogsTime = function(time) {
+      const now = moment();
+      switch (time) {
+        case 'ONE_DAY':
+          $scope.fromDate = now.add(-1, 'day').format();
+          $scope.timeLabel = ONE_DAY;
+          break;
+        case 'TWO_DAYS':
+          $scope.fromDate = now.add(-2, 'days').format();
+          $scope.timeLabel = TWO_DAYS;
+          break;
+        case 'THREE_DAYS':
+          $scope.fromDate = now.add(-3, 'days').format();
+          $scope.timeLabel = THREE_DAYS;
+          break;
+        case 'ONE_WEEK':
+          $scope.fromDate = now.add(-1, 'week').format();
+          $scope.timeLabel = ONE_WEEK;
+          break;
+        case 'TWO_WEEKS':
+          $scope.fromDate = now.add(-2, 'weeks').format();
+          $scope.timeLabel = TWO_WEEKS;
+          break;
+        case 'ONE_MONTH':
+          $scope.fromDate = now.add(-1, 'month').format();
+          $scope.timeLabel = ONE_MONTH;
+          break;
+        case 'TWO_MONTHS':
+          $scope.fromDate = now.add(-2, 'months').format();
+          $scope.timeLabel = TWO_MONTHS;
+          break;
+        case 'THREE_MONTHS':
+          $scope.fromDate = now.add(-3, 'months').format();
+          $scope.timeLabel = THREE_MONTHS;
+          break;
+        default:
+          $scope.fromDate = now.add(-1, 'day').format();
+          $scope.timeLabel = ONE_DAY;
+      }
+      $scope.time = time;
+      this.getElastilogs();
     };
 
     this.getElastilogs = function() {
@@ -34,9 +121,9 @@ module(SPOT_SERVERGROUP_DETAILS_ELASTILOGS_CONTROLLER, []).controller('spotElast
       ServerGroupReader.getElastilogs(
         this.serverGroup,
         this.serverGroup.elastigroup.id,
-        '2021-05-09T00:00:00.000+00:00',
-        '2021-05-23T00:00:00.000+00:00',
-        'INFO',
+        $scope.fromDate,
+        $scope.toDate,
+        $scope.severity,
       ).then(
         elastilogs => {
           this.viewState.loading = false;
