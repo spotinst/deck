f914fc9af45267bbd2c8e68d56d8ce2965d9c418
serhii.kryvokin <serh.kryvokin@gmail.com>
2021-05-29 07:44:52 +0300

    added all the details and actions

--- app/scripts/modules/spot/src/instance/details/instance.details.controller.js
+++ app/scripts/modules/spot/src/instance/details/instance.details.controller.js
@@ -2,8 +2,9 @@
 
 import { module } from 'angular';
 
-import { INSTANCE_WRITE_SERVICE } from '@spinnaker/core';
+import { ConfirmationModalService, INSTANCE_WRITE_SERVICE, InstanceReader } from '@spinnaker/core';
 import UIROUTER_ANGULARJS from '@uirouter/angularjs';
+import _ from 'lodash';
 
 export const SPOT_INSTANCE_DETAILS_INSTANCE_DETAILS_CONTROLLER = 'spinnaker.spot.instance.details.controller';
 export const name = SPOT_INSTANCE_DETAILS_INSTANCE_DETAILS_CONTROLLER; // for backwards compatibility
@@ -11,11 +12,12 @@ module(SPOT_INSTANCE_DETAILS_INSTANCE_DETAILS_CONTROLLER, [UIROUTER_ANGULARJS, I
   'spotInstanceDetailsCtrl',
   [
     '$scope',
+    '$state',
     '$q',
     'instanceWriter',
     'app',
     'instance',
-    function($scope, $q, instanceWriter, app, instance) {
+    function($scope, $state, $q, instanceWriter, app, instance) {
       $scope.application = app;
 
       const initialize = app.isStandalone
@@ -47,8 +49,79 @@ module(SPOT_INSTANCE_DETAILS_INSTANCE_DETAILS_CONTROLLER, [UIROUTER_ANGULARJS, I
           });
         }
 
-        $scope.instance = instanceSummary;
+        instanceSummary.account = account;
+        instanceSummary.region = region;
+        instanceSummary.instanceId = instance.instanceId;
+        if (instanceSummary && account && region) {
+          return InstanceReader.getInstanceDetails(account, region, instance.instanceId).then(details => {
+            $scope.instance = _.defaults(instanceSummary, details);
+            $scope.instance.serverGroupId = details.serverGroup;
+            const date = new Date(details.launchTime);
+
+            $scope.instance.launchTime = date.toLocaleString();
+          });
+        }
       }
+
+      this.terminateInstance = function terminateInstance() {
+        const instance = $scope.instance;
+
+        const taskMonitor = {
+          application: app,
+          title: 'Terminating ' + instance.instanceId,
+          onTaskComplete: function() {
+            if ($state.includes('**.instanceDetails', { instanceId: instance.instanceId })) {
+              $state.go('^');
+            }
+          },
+        };
+
+        const submitMethod = function() {
+          const params = { cloudProvider: 'spot' };
+
+          params.elastiGroup = instance.serverGroupId;
+
+          return instanceWriter.terminateInstance(instance, app, params);
+        };
+
+        ConfirmationModalService.confirm({
+          header: 'Really terminate ' + instance.instanceId + '?',
+          buttonText: 'Terminate ' + instance.instanceId,
+          account: instance.account,
+          taskMonitorConfig: taskMonitor,
+          submitMethod: submitMethod,
+        });
+      };
+
+      this.terminateInstanceAndShrinkServerGroup = function terminateInstanceAndShrinkServerGroup() {
+        const instance = $scope.instance;
+
+        const taskMonitor = {
+          application: app,
+          title: 'Terminating ' + instance.instanceId + ' and shrinking server group',
+          onTaskComplete: function() {
+            if ($state.includes('**.instanceDetails', { instanceId: instance.instanceId })) {
+              $state.go('^');
+            }
+          },
+        };
+
+        const submitMethod = function() {
+          const params = { cloudProvider: 'spot' };
+          params.elastiGroup = instance.serverGroupId;
+          params.instanceIds = [instance.id];
+
+          return instanceWriter.terminateInstanceAndShrinkServerGroup(instance, app, params);
+        };
+
+        ConfirmationModalService.confirm({
+          header: 'Really terminate ' + instance.instanceId + ' and shrink ' + instance.serverGroup + '?',
+          buttonText: 'Terminate ' + instance.instanceId + ' and shrink ' + instance.serverGroup,
+          account: instance.account,
+          taskMonitorConfig: taskMonitor,
+          submitMethod: submitMethod,
+        });
+      };
     },
   ],
 );
